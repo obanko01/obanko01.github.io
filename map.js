51bee6acb43b5f2603a9472351c0541436f38b3a
obanko01 <oluseye.bankole@tufts.edu>
2014-03-12 19:59:37 -0400

    ..

--- map.js
+++ map.js
@@ -2,7 +2,7 @@
 
 function initialize(){
 	var mapOptions = {
-		center: new google.maps.LatLng(-34.397, 150.644), zoom: 8
+		zoom: 8
 	};
 		var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 	}
