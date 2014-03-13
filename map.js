ad35ef4e31ac1b475c38be1c76023fe423a8a6ce
obanko01 <oluseye.bankole@tufts.edu>
2014-03-12 23:24:01 -0400

    .

--- map.js
+++ map.js
@@ -1,19 +1,16 @@
 //src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=TRUE";
 var xhr;
 function initialize(){
-	/*var mapOptions = {
+	var mapOptions = {
 		zoom: 8
 	};
-	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);*/
-
+	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 	xhr = new XMLHttpRequest();
 	//xhr.open("method", "url", asynchronous? true or false);
 	xhr.open("GET", "http://mbta.herokuapp.com/mapper/rodeo.json", true);
 	//onreadystatechange has to be set to a function when request is completed to handle the response
 	xhr.onreadystatechange = dataReady;
 	xhr.send(null); //Go! Execute!
-
-
 }
 
 
