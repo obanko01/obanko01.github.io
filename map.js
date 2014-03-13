bf9cb9c1223dd26e2e6da8706a3df2b5288da750
obanko01 <oluseye.bankole@tufts.edu>
2014-03-13 00:40:03 -0400

    .

--- map.js
+++ map.js
@@ -1,25 +1,44 @@
-//src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=TRUE";
-var xhr;
-function initialize(){
+
+	var xhr; 
+
+	function init() {
+
+	var location = new google.maps.LatLng(42.3599611, -71.0567528);
 	var mapOptions = {
-		zoom: 8
+		zoom: 8, center: location, mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+//create a marker
+
+var marker = new google.maps.Marker({
+		position:location,
+		title: "Faneuil Hall, Boston, MA"
+})
 	xhr = new XMLHttpRequest();
-	//xhr.open("method", "url", asynchronous? true or false);
-	xhr.open("GET", "http://mbta.herokuapp.com/mapper/rodeo.json", true);
-	//onreadystatechange has to be set to a function when request is completed to handle the response
+	xhr.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true); // this is possible because of cross-origin resource sharing (CORS) enabled for web application
+
+		// onreadystatechange has to be set to a...
+		// ...function when request is completed, to...
+		// ...handle the response
 	xhr.onreadystatechange = dataReady;
-	xhr.send(null); //Go! Execute!
-}
+	xhr.send(null); // Go! Execute!
+	}
 
-
-function dataReady(){
-	if (xhr.readystate == 4 && xhr.status == 200){
-	console.log("DONE!")
-	scheduleData = JSON.parse(xhr.responseText);
-	console.log(schedule);
-	scheduleDom = document.getElementbyId("map-canvas");
-	scheduleDom.innerHTML = scheduleData["line"];}
-	//for the assigment can get away with 500 status code else do an "else statement" to catch this error}
-}
+	function dataReady() {
+		// The readyState numbers:
+		// 0 = not initialized
+		// 1 = Set up
+		// 2 = Sent
+		// 3 = In progress
+		// 4 = Complete
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			scheduleData = JSON.parse(xhr.responseText);
+			scheduleDom = document.getElementById("schedule");
+			scheduleDom.innerHTML = scheduleData["line"];
+		}
+		else if (xhr.readyState == 4 && xhr.status == 500) {
+			scheduleDom = document.getElementById("schedule");
+			scheduleDom.innerHTML = '<p><img src="http://www.yiyinglu.com/failwhale/images/Homer_the_New_Fail_Whale_by_edwheeler.jpg" alt="fail" /></p>';
+			
+		}
+	}
